dd3c28712f09e93b7a49e7f2517ca06ad41adfd8
Ramsay Leung <ramsayleung@gmail.com>
2024-01-14 19:52:47 -0800

    Extract CrewGroupView and DivdierView.

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -7,11 +7,57 @@
 
 import SwiftUI
 
-struct MissionView: View {
-    struct CrewMember {
-        let role: String
-        let astronaut: Astronaut
+struct CrewMember {
+    let role: String
+    let astronaut: Astronaut
+}
+
+struct CrewGroupView: View {
+    let crews: [CrewMember]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(crews, id: \.role) { crew in
+                    NavigationLink {
+                        AstronautView(astronaut: crew.astronaut)
+                    } label: {
+                        HStack {
+                            Image(crew.astronaut.id)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width:  100, height: 72)
+                                .clipShape(Capsule())
+                                .overlay(Capsule()
+                                    .strokeBorder(.white, lineWidth: 1))
+                            
+                            VStack(alignment: .leading) {
+                                Text(crew.astronaut.name)
+                                    .font(.headline)
+                                    .foregroundStyle(.white)
+                                
+                                Text(crew.role)
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+            }
+        }
     }
+}
+
+struct DivdierView: View {
+    var body: some View {
+        Rectangle()
+            .frame(height: 2)
+            .foregroundColor(.lightBackground)
+            .padding(.vertical)
+    }
+}
+
+struct MissionView: View {
     
     let crews: [CrewMember]
     let mission: Mission
@@ -35,11 +81,14 @@ struct MissionView: View {
                     .containerRelativeFrame([.horizontal]){width, axis in
                         width * 0.6
                     }
+                HStack {
+                    Text("Launch on: ")
+                    Text(mission.displayLaunchedDate)
+                        .foregroundColor(mission.displayLaunchedDate == "N/A" ? .gray : .white)
+                }
+                .padding()
                 VStack(alignment: .leading){
-                    Rectangle()
-                        .frame(height: 2)
-                        .foregroundColor(.lightBackground)
-                        .padding(.vertical)
+                    DivdierView()
                     
                     Text("Mission Highlights")
                         .font(.title.bold())
@@ -47,46 +96,15 @@ struct MissionView: View {
                     
                     Text(mission.description)
                     
-                    Rectangle()
-                        .frame(height: 2)
-                        .foregroundColor(.lightBackground)
-                        .padding(.vertical)
+                    DivdierView()
                     
                     Text("Crew")
                         .font(.title.bold())
                     
                 }
                 .padding(.horizontal)
-                
-                ScrollView(.horizontal, showsIndicators: false){
-                    HStack {
-                        ForEach(crews, id: \.role) { crew in
-                            NavigationLink {
-                                AstronautView(astronaut: crew.astronaut)
-                            } label: {
-                                HStack {
-                                    Image(crew.astronaut.id)
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width:  100, height: 72)
-                                        .clipShape(Capsule())
-                                        .overlay(Capsule()
-                                            .strokeBorder(.white, lineWidth: 1))
-                                    
-                                    VStack(alignment: .leading) {
-                                        Text(crew.astronaut.name)
-                                            .font(.headline)
-                                            .foregroundStyle(.white)
-                                        
-                                        Text(crew.role)
-                                            .foregroundStyle(.secondary)
-                                    }
-                                }
-                                .padding(.horizontal)
-                            }
-                        }
-                    }
-                }
+
+                CrewGroupView(crews: crews)
                 
             }
             .padding(.bottom)
